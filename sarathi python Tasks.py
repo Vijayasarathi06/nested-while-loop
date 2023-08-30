c69d6cf25eeed6783ccc5a04b1764fb0e12b2e7b
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-08-30 10:37:13 +0530

    nested while loop Task

---
+++ sarathi python Tasks.py
@@ -0,0 +1,88 @@
+##Even numbers
+i=2
+while(i<=10):
+    print(i)
+    i+=2
+##Odd number
+i=1
+while(i<=10):
+    print(i)
+    i+=2
+##Fibonacci series
+##num=int(input("enter the fibonacci series"))
+a=0
+b=1
+i=0
+while(i<=num):
+    print(b,end=" ")
+    c=a+b
+    a=b
+    b=c
+    i+=1
+    print()
+##Natural numbers
+i=0
+while(i<=10):
+    print(i)
+    i+=1
+##Numbers multipy of five
+i=1
+while(i<=20):
+    print(i)
+    i+=1
+    if(i%5==0):
+        print("sarathi")
+else:
+        print()
+##Tables:
+T=int(input("enter the tables"))
+i=1
+while(i<=10):
+    print(i,"X",T,"=",T*i)
+    i+=1
+print()
+##Sum of the numbers
+i=2
+while(i<=20):
+    print(i)
+    i+=1
+##PATTERN `1
+i=1
+while(i<=3):
+    print(end=" " )
+    i+=1
+    j=1
+    while(j<=3):
+        print(j,end=" ")
+        j+=1.
+        
+    print()
+##PATTERN 2
+i=65
+while(i<=67):
+    print(end=" ")
+    i+=1
+    j=65
+    while(j<=67):
+        print(chr(j),end=" ")
+        j+=1
+    print()
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
